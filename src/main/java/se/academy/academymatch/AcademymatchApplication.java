c7315a9b31ac9fa75ab3e22b1aa4fff2afa3334e
Christian Enarsson <christianenarsson2@hotmail.com>
2017-11-21 10:40:18 +0100

    Started project

---
+++ src/main/java/se/academy/academymatch/AcademymatchApplication.java
@@ -0,0 +1,12 @@
+package se.academy.academymatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademymatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademymatchApplication.class, args);
+	}
+}
