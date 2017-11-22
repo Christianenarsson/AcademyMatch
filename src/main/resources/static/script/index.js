d0652a19acb8296e30231f5daa0055a17d4ce487
joonsson <joonsson@gmail.com>
2017-11-22 11:17:27 +0100

    Added pref selector + logic

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,7 +1,28 @@
 var startBtn = document.getElementById("startbtn");
+var preferences = document.getElementById("preferences");
+var httpRequest;
 
 startBtn.addEventListener("click", start);
 
 function start() {
-    document.location.href = "/loading";
+    var chosenPref = preferences.options[preferences.selectedIndex].value;
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        console.log('There was a problem making the request.');
+    }
+    httpRequest.onreadystatechange = go;
+    httpRequest.open('POST', '/setPref', true);
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.send("preference=" + encodeURIComponent(chosenPref));
+}
+
+function go() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            document.location.href = "/loading";
+        } else {
+            console.log('There was a problem receiving the request.');
+            start();
+        }
+    }
 }
